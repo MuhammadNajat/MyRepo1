c1266610b9c623f80be6d9d14116a694905d21d4
Najat <noreply@mymail.com>
2017-07-28 12:33:15 +0600

    Added Rolling File Appended Error Logger

--- TryMVC1/TryMVC1/Controllers/HomeController.cs
+++ TryMVC1/TryMVC1/Controllers/HomeController.cs
@@ -3,13 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using log4net;
 
 namespace TryMVC1.Controllers
 {
     public class HomeController : Controller
     {
+
+        private static readonly ILog log = LogManager.GetLogger("DemoLog");
+
         public ActionResult Index()
         {
+
+            try
+            {
+                int x = 1000000;
+                for (int i = -10; i <= 10; i++)
+                    x /= i;
+            }
+
+            catch(Exception ex)
+            {
+                log.Debug("Error Occured in Index", ex);
+            }
+
+            //log.Debug("HeLLO DEBUGGING!!!");
+
             return View();
         }
 
